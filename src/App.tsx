f005964947bbb7fba43ccfbcd4e9537db2823e70
Henrique Santos <silvashrq@gmail.com>
2025-11-08 20:30:30 -0300

    Terceiro Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./styles/global.css";
 
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   return (
@@ -11,7 +13,36 @@ export function App() {
       <Container>
         <Logo />
       </Container>
-      {/* <Container></Container> */}
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 </p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
